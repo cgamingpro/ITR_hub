0f2926a381dbc7ac37e303a23c0c41c97a024ec8
cgamingpro <hariomjangra777@gmail.com>
2026-04-07 19:25:59 +0530

    Update auth.py

--- FrontEnd/auth.py
+++ FrontEnd/auth.py
@@ -13,7 +13,7 @@ rail = os.getenv("ENV")
 if rail is not None and rail == "RAILWAY":
     SECRET_KEY = os.getenv("SECRET_KEY")
     ALGORITHM = os.getenv("ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_eXPIRE_MINUTES"))
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 else:
     import local_config
